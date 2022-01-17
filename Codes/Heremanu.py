acb434caa8610b2ee1e61042531fc96428540005
p.jezequel <p.jezequel@lacroixrouge-brest.fr>
2022-01-17 17:21:05 +0000

    Retour en arriere parce que j'ai fait de la merde

--- Codes/Heremanu.py
+++ Codes/Heremanu.py
@@ -4,7 +4,7 @@ def creer_nom_sauvegarde(nom:str, caract:tuple)->str:
     à sauvegarder, en relation avec le nom initial
     """
     assert type(nom)==str, "Il n'y a pas de nom pour le fichier"  
-    new_file = nom.replace(".png", "_new.png")
+    new_file = nom.replace(".jpg", "_new.png")
     return new_file
 
     
